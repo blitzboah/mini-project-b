9bc154c1863956c3ac0635a8f3a42dd103ef5f62
NeeL1249 <neelmulik12@gmail.com>
2024-03-12 21:07:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
