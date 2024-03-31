2c2def8bd8e66bbd153d1df8e31615b0dc4d9b6a
blitzboi <146631108+blitz31@users.noreply.github.com>
2024-03-31 08:08:01 +0000

    index

---
+++ frontend/busInfo_frontend/src/App.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faUsers, faHeart } from '@fortawesome/free-solid-svg-icons';
+
+function Navbar() {
+  return (
+    <nav className="flex justify-center items-center flex-wrap bg-gray-900 p-4">
+      <a className="text-yellow-300 underline-none mr-auto text-xl" href="#">BusInfo</a>
+      <div className="flex justify-center items-center space-x-4">
+        <a className="text-white hover:text-yellow-300 underline-none" href="#">Home</a>
+        <a className="text-white hover:text-yellow-300 underline-none" href="#">Register</a>
+        <a className="text-white hover:text-yellow-300 underline-none" href="#">View</a>
+        <a className="text-white hover:text-yellow-300 underline-none" href="#">Logout</a>
+      </div>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <section className="bg-gray-700 text-white p-10">
+      <div className="container mx-auto flex flex-wrap items-center">
+        <div className="w-full md:w-1/2">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">For Efficiently Storing Your Data and get Rid of Paperwork</h1>
+        </div>
+        <div className="w-full md:w-1/2 flex justify-center">
+          <img className="max-w-lg h-auto" src="images/bus-picture.png" alt="iphone-mockup" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Features() {
+  return (
+    <section className="bg-gray-500 text-white p-8">
+      <div className="container mx-auto py-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="feature-box text-center">
+            <FontAwesomeIcon icon={faCheckCircle} className="text-6xl text-gray-900" />
+            <h3 className="text-xl font-semibold mt-4">Easy to use</h3>
+            <p className="text-white mt-2">So easy to use that anyone can handle their data safely and efficiently.</p>
+          </div>
+          <div className="feature-box text-center">
+            <FontAwesomeIcon icon={faUsers} className="text-6xl text-gray-900" />
+            <h3 className="text-xl font-semibold mt-4">Work as a Team</h3>
+            <p className="text-white mt-2">At BusInfo we work as a team try to deliver the best possible features.</p>
+          </div>
+          <div className="feature-box text-center">
+            <FontAwesomeIcon icon={faHeart} className="text-6xl text-gray-900" />
+            <h3 className="text-xl font-semibold mt-4">User Friendly</h3>
+            <p className="text-white mt-2">Our aim is to create a User Friendly website for your comfort, as customers are our first priority.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function CTA() {
+  return (
+    <section className="colored-section" id="cta">
+      <div className="container-fluid">
+        <h3 className="service-heading">Get the Right Service for your Business</h3>
+        <p className="service" style={{ fontSize: '1.15rem' }}>
+          We provide tracking of license expiry date of all the drivers registered with your agency as well we keep track of the permit of the vehicles used by your agency.<br />
+          As the world is moving at fast pace and everyone is getting adapted to new technologies it is, Your time to use them and get rid of all the old methods of storing your data.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer id="footer" className="container-fluid white-section">
+      <button className="btn btn-lg"><i className="fa-brands fa-twitter"></i></button>
+      <button className="btn btn-lg"><i className="fa-brands fa-facebook"></i></button>
+      <button className="btn btn-lg"><i className="fa-brands fa-instagram"></i></button>
+      <button className="btn btn-lg"><i className="fa-solid fa-envelope "></i></button>
+      <p>© Copyright BusInfo</p>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Features />
+      <CTA />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
